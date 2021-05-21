c649a49c54edc1fc463678bacfbb8d3986c54044
Devon <55559667+K1ngsJester@users.noreply.github.com>
2021-05-20 21:25:39 -0700

    added functions for steps and calories burned

--- app/src/main/java/com/personalchef/mealplan/MainActivity.java
+++ app/src/main/java/com/personalchef/mealplan/MainActivity.java
@@ -2,13 +2,45 @@ package com.personalchef.mealplan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private double MagnitudePrevious = 0;
+    public Integer stepCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        SensorEventListener stepDetector = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if (sensorEvent != null) {
+                    float x_acceleration = sensorEvent.values[0];
+                    float y_acceleration = sensorEvent.values[1];
+                    float z_acceleration = sensorEvent.values[2];
+                    double Magnitude = Math.sqrt(x_acceleration * x_acceleration + y_acceleration * y_acceleration + z_acceleration * z_acceleration);
+                    double MagnitudeDelta = Magnitude - MagnitudePrevious;
+                    MagnitudePrevious = Magnitude;
+
+                    if (MagnitudeDelta > 4) {
+                        stepCount++;
+                        //add an update to a graph or other visual progress function as a stretch goal
+                    }
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
+        sensorManager.registerListener(stepDetector, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
     }
 }
--- app/src/main/java/com/personalchef/mealplan/models/StepCounter.java
+++ app/src/main/java/com/personalchef/mealplan/models/StepCounter.java
@@ -1,30 +1,74 @@
 package com.personalchef.mealplan.models;
 
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.personalchef.mealplan.MainActivity;
+
 import java.util.Date;
+
 
 /***
  * StepCounter
  *
  * SensorAdapter updates the StepCount
  */
-public class StepCounter {
-    private int stepCounter;
+public class StepCounter extends AppCompatActivity {
     private Date date;
+    private int weight = 180; //lbs
+    private int height = 72; //inches
+    private double stepLength = height*.42/12; //used to get user's stepLength
+    private double caloriePerMile = weight*.57; //based on causal walking speed
+    private double stepsPerMile = 5280/stepLength;
+    private Integer caloriesBurned = 0;
+
+
 
     // Functionality
-    public int GetStepCount() {
-        int steps = 0;
+    public int GetStepCount(int stepCount) {
         // Get step count from sensor, &/or Db
         // Return sum of all
-        return steps;
+        return stepCount;
     }
 
-    public StepCalorieDetails CalculateCaloriesBurnt() {
+    public int  CalculateCaloriesBurnt(int stepCount) {
         StepCalorieDetails scDetail = new StepCalorieDetails();
         // Get step count
         // Get calories from all day/week meals
         // Calculates calorie details
+        caloriesBurned = (int) Math.floor((caloriePerMile / stepsPerMile) * stepCount * 100); // 91.2 / 2138 = cal per step then multiply by stepCount. 100 only used for testing purposes
 
-        return scDetail;
+        return caloriesBurned;
+    }
+    protected void onPause(int stepCount) {
+        super.onPause();
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.putInt("stepCount", stepCount);
+        editor.apply();
+    }
+
+    protected void onStop(int stepCount) {
+        super.onStop();
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.putInt("stepCount", stepCount);
+        editor.apply();
+    }
+
+    protected void onResume(int stepCount) {
+        super.onResume();
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        stepCount = sharedPreferences.getInt("stepCount", 0);
     }
 }
