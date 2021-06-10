0319c538707d2fea24986fdf74e8864288655d11
Alejandra <30509870+avalencia0512@users.noreply.github.com>
2021-06-10 14:08:14 -0700

    added drawer button

--- app/src/main/java/com/personalchef/mealplan/MainActivity.java
+++ app/src/main/java/com/personalchef/mealplan/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.navigation.NavigationView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,11 +22,12 @@ import androidx.fragment.app.FragmentTransaction;
 import com.personalchef.mealplan.models.DatabaseHelper;
 import com.personalchef.mealplan.models.User;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DatabaseHelper helper = null;
     private StepCounterActivity sc = new StepCounterActivity();
-
-    public DrawerLayout drawerLayout;
+    public DrawerLayout drawer;
+    public ActionBarDrawerToggle toggle;
+    public NavigationView navView;
 
     public MainActivity() {
         // With final init, error of SQL not closing was generated. Init in constructor
@@ -39,6 +41,13 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.my_drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.nav_open, R.string.nav_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         // Load user from file
         User u = IOHelper.loadUserFromFile(getApplicationContext()) ;
 
@@ -79,5 +88,29 @@ public class MainActivity extends AppCompatActivity {
     public void stepGoalSubmitted(View view) {
         Intent intent = new Intent(getApplicationContext(), StepCounterActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Fragment fragment = null;
+        Intent intent = null;
+
+        switch (id)
+        {
+            case R.id.userProfile:
+                intent = new Intent(getApplicationContext(), UserProfileActivity.class);
+                break;
+
+            case R.id.setStepGoal:
+                intent = new Intent(getApplicationContext(), SetStepGoal.class);
+                break;
+
+            case R.id.stepCounter:
+                intent = new Intent(getApplicationContext(), StepCounterActivity.class);
+                break;
+        }
+        startActivity(intent);
+        return false;
     }
 }
