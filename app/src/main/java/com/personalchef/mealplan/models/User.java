bb308bdf02456477157280a5210341769e809ee2
Alejandra <30509870+avalencia0512@users.noreply.github.com>
2021-06-18 00:25:18 -0700

    step goal is saved and displayed in step counter details

--- app/src/main/java/com/personalchef/mealplan/models/User.java
+++ app/src/main/java/com/personalchef/mealplan/models/User.java
@@ -5,19 +5,13 @@ import java.io.Serializable;
 public class User implements Serializable {
     public static String EXTRA_USEROBJ = "com.personalchef.mealplan.models.User";
 
-    private String userName;
-    private String hash;
+
     private int weight;
     private float height;
     private int age;
 
-    public User() {
-        userName = "chefUser";
-        hash = "4b5v4398573406";
-    }
 
     public User(int weight, float height, int age) {
-        this();
         this.weight = weight;
         this.height = height;
         this.age = age;
@@ -31,24 +25,10 @@ public class User implements Serializable {
     }
 
     public int getWeight() {
-        return this.weight;
+        return weight;
     }
     public void setWeight(int _weight) {
         this.weight = _weight;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getHash() {
-        return hash;
-    }
-    public  void setHash(String hash) {
-        this.hash = hash;
     }
 
     public int getAge() {
@@ -58,8 +38,11 @@ public class User implements Serializable {
         this.age = age;
     }
 
+
+
+
     public String toString() {
-        return "UserName: " + this.userName + " Hash: " + this.hash +
+        return /*"UserName: " + this.userName + " Hash: " + this.hash +*/
                 " Weight: " + this.weight + " Height: " + this.height +
                 " Age: " + this.age;
     }
